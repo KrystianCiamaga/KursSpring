7d9ca8c3093dfc62de4927713ad9986923c9f01a
KrzysztofKrocz <Krzysztof.Krocz@o2.pl>
2019-02-05 21:09:27 +0100

    JSTL + Exception handling

---
+++ src/pl/javasolution/pojo/Product.java
@@ -0,0 +1,30 @@
+package pl.javasolution.pojo;
+
+public class Product {
+
+	private String category;
+	private String name;
+
+	public Product(String category, String name) {
+		super();
+		this.category = category;
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
