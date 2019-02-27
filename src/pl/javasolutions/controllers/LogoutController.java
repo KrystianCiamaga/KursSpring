aa88976fd396db624bef1b72caf0b4382c0d5ee2
krzysztofkrocz <Krzysztof.Krocz@o2.pl>
2019-02-27 20:24:28 +0100

    Spring MVC 4 - Sesja

---
+++ src/pl/javasolutions/controllers/LogoutController.java
@@ -0,0 +1,18 @@
+package pl.javasolutions.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+
+	@GetMapping(value = "/logout")
+	public String userPage(Model model, HttpSession session) {
+
+		session.invalidate();
+		return "redirect:/";
+	}
+}
